11aaa6d9e86c71a054f0bf0812754214d6416c3a
Zeeshan Ahmad <zishanahmad114477@gmail.com>
2024-08-21 03:51:22 +0500

    added splash screen

---
+++ lib/screens/splashscreen.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
+
+class Splashscreen extends StatefulWidget {
+  const Splashscreen({super.key});
+
+  @override
+  State<Splashscreen> createState() => _SplashscreenState();
+}
+
+class _SplashscreenState extends State<Splashscreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller =
+        AnimationController(duration: const Duration(seconds: 10), vsync: this)
+          ..repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          AnimatedBuilder(
+              animation: _controller,
+              child: Center(
+                child: SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.6,
+                  width: MediaQuery.of(context).size.width * 0.6,
+                  child: Center(child: Image.asset('assets/virus.png')),
+                ),
+              ),
+              builder: (BuildContext context, Widget? child) {
+                return Transform.rotate(
+                  angle: _controller.value * 2.0 * math.pi,
+                  child: child,
+                );
+              }),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.02,
+          ),
+          const Text(
+            '   Covid-19 \nTracker App',
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          )
+        ],
+      )),
+    );
+  }
+}
