a9fdd4af419cf69374da171ca50edf6be4da98d3
Zeeshan Ahmad <zishanahmad114477@gmail.com>
2024-08-22 02:11:09 +0500

    integrated world states api

--- lib/screens/states_screen.dart
+++ lib/screens/states_screen.dart
@@ -1,4 +1,7 @@
+import 'package:covid_tracker/Model/world_states_model.dart';
+import 'package:covid_tracker/services/states_services.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:pie_chart/pie_chart.dart';
 
 class StatesScreen extends StatefulWidget {
@@ -26,130 +29,177 @@ class _StatesScreenState extends State<StatesScreen>
 
   @override
   Widget build(BuildContext context) {
+    StatesServices statesServices = StatesServices();
     final screenHeight = MediaQuery.of(context).size.height;
     final screenWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
       backgroundColor: const Color.fromARGB(49, 238, 238, 238),
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(height: screenHeight * 0.05),
-              // Pie Chart Section
-              Card(
-                elevation: 8,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
+        child: FutureBuilder(
+            future: statesServices.fetchWorldStatesApi(),
+            builder: (context, AsyncSnapshot<WorldStatesModel> snapshot) {
+              if (!snapshot.hasData) {
+                return const Expanded(
+                    flex: 1,
+                    child: SpinKitFadingCircle(
+                      color: Colors.white,
+                      size: 50,
+                    ));
+              } else {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
                   child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      const Text(
-                        "COVID-19 Statistics",
-                        style: TextStyle(
-                          fontSize: 22,
-                          fontWeight: FontWeight.bold,
+                      SizedBox(height: screenHeight * 0.05),
+                      // Pie Chart Section
+                      Card(
+                        elevation: 8,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
                         ),
-                      ),
-                      SizedBox(height: screenHeight * 0.02),
-                      PieChart(
-                        dataMap: const {
-                          'Total': 100,
-                          'Recovered': 30,
-                          'Deaths': 20,
-                        },
-                        animationDuration: const Duration(milliseconds: 800),
-                        chartType: ChartType.ring,
-                        colorList: const [
-                          Colors.blue,
-                          Colors.green,
-                          Colors.red,
-                        ],
-                        chartRadius: screenWidth / 2.8,
-                        chartValuesOptions: const ChartValuesOptions(
-                          showChartValuesInPercentage: true,
-                          showChartValuesOutside: false,
-                          chartValueBackgroundColor: Colors.transparent,
-                          chartValueStyle: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Column(
+                            children: [
+                              const Text(
+                                "COVID-19 Statistics",
+                                style: TextStyle(
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              SizedBox(height: screenHeight * 0.02),
+                              PieChart(
+                                dataMap: {
+                                  'Total': double.parse(
+                                      snapshot.data!.cases.toString()),
+                                  'Recovered': double.parse(
+                                      snapshot.data!.recovered.toString()),
+                                  'Deaths': double.parse(
+                                      snapshot.data!.deaths.toString()),
+                                },
+                                animationDuration:
+                                    const Duration(milliseconds: 800),
+                                chartType: ChartType.ring,
+                                colorList: const [
+                                  Colors.blue,
+                                  Colors.green,
+                                  Colors.red,
+                                ],
+                                chartRadius: screenWidth / 2.8,
+                                chartValuesOptions: const ChartValuesOptions(
+                                  showChartValuesInPercentage: true,
+                                  showChartValuesOutside: false,
+                                  chartValueBackgroundColor: Colors.transparent,
+                                  chartValueStyle: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                legendOptions: const LegendOptions(
+                                  legendPosition: LegendPosition.left,
+                                  legendShape: BoxShape.circle,
+                                  showLegendsInRow: false,
+                                  legendTextStyle: TextStyle(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
                         ),
-                        legendOptions: const LegendOptions(
-                          legendPosition: LegendPosition.left,
-                          legendShape: BoxShape.circle,
-                          showLegendsInRow: false,
-                          legendTextStyle: TextStyle(
-                            fontSize: 14,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
                       ),
-                    ],
-                  ),
-                ),
-              ),
-              SizedBox(height: screenHeight * 0.04),
-              // Information Section
-              Expanded(
-                child: SingleChildScrollView(
-                  child: Card(
-                    elevation: 8,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: const Padding(
-                      padding: EdgeInsets.all(16.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            "Country Details",
-                            style: TextStyle(
-                              fontSize: 22,
-                              fontWeight: FontWeight.bold,
+                      SizedBox(height: screenHeight * 0.04),
+                      // Information Section
+                      Expanded(
+                        child: SingleChildScrollView(
+                          child: Card(
+                            elevation: 8,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  const Text(
+                                    " Details",
+                                    style: TextStyle(
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 10),
+                                  ReuseableRow(
+                                    title: 'Total',
+                                    value: snapshot.data!.cases.toString(),
+                                  ),
+                                  ReuseableRow(
+                                    title: 'Recovered',
+                                    value: snapshot.data!.recovered.toString(),
+                                  ),
+                                  ReuseableRow(
+                                    title: 'Deaths',
+                                    value: snapshot.data!.deaths.toString(),
+                                  ),
+                                  ReuseableRow(
+                                    title: 'Critical',
+                                    value: snapshot.data!.critical.toString(),
+                                  ),
+                                  ReuseableRow(
+                                    title: 'Active',
+                                    value: snapshot.data!.active.toString(),
+                                  ),
+                                  ReuseableRow(
+                                    title: 'AffectedCountries',
+                                    value: snapshot.data!.affectedCountries
+                                        .toString(),
+                                  ),
+                                  ReuseableRow(
+                                    title: 'TodayCases',
+                                    value: snapshot.data!.todayCases.toString(),
+                                  ),
+                                  ReuseableRow(
+                                    title: 'TodayDeaths',
+                                    value:
+                                        snapshot.data!.todayDeaths.toString(),
+                                  ),
+                                  ReuseableRow(
+                                    title: 'TodayRecovere',
+                                    value: snapshot.data!.todayRecovered
+                                        .toString(),
+                                  ),
+                                ],
+                              ),
                             ),
                           ),
-                          SizedBox(height: 10),
-                          ReuseableRow(title: 'Pakistan', value: '47'),
-                          ReuseableRow(title: 'India', value: '85'),
-                          ReuseableRow(title: 'USA', value: '120'),
-                          ReuseableRow(title: 'UK', value: '76'),
-                          ReuseableRow(title: 'Brazil', value: '54'),
-                          ReuseableRow(title: 'Brazil', value: '54'),
-                          ReuseableRow(title: 'Brazil', value: '54'),
-                          ReuseableRow(title: 'Brazil', value: '54'),
-                          ReuseableRow(title: 'Brazil', value: '54'),
-                          ReuseableRow(title: 'Brazil', value: '54'),
-                          ReuseableRow(title: 'Brazil', value: '54'),
-                          ReuseableRow(title: 'Brazil', value: '54'),
-                        ],
+                        ),
                       ),
-                    ),
+                      SizedBox(height: screenHeight * 0.05),
+                      Container(
+                        height: screenHeight * 0.05,
+                        width: screenWidth * 0.8,
+                        decoration: const BoxDecoration(
+                            color: Color.fromARGB(255, 19, 162, 33),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(12))),
+                        child: const Center(
+                            child: Text(
+                          'Track Countries',
+                          style: TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.bold),
+                        )),
+                      ),
+                      SizedBox(height: screenHeight * 0.04),
+                    ],
                   ),
-                ),
-              ),
-              SizedBox(height: screenHeight * 0.05),
-              Container(
-                height: screenHeight * 0.05,
-                width: screenWidth * 0.8,
-                decoration: const BoxDecoration(
-                    color: Color.fromARGB(255, 19, 162, 33),
-                    borderRadius: BorderRadius.all(Radius.circular(12))),
-                child: const Center(
-                    child: Text(
-                  'Track Countries',
-                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-                )),
-              ),
-              SizedBox(height: screenHeight * 0.04),
-            ],
-          ),
-        ),
+                );
+              }
+            }),
       ),
     );
   }
---
+++ lib/services/states_services.dart
@@ -0,0 +1,16 @@
+import 'dart:convert';
+import 'package:covid_tracker/Model/world_states_model.dart';
+import 'package:covid_tracker/services/utilities/api_url.dart';
+import 'package:http/http.dart' as http;
+
+class StatesServices {
+  Future<WorldStatesModel> fetchWorldStatesApi() async {
+    final response = await http.get(Uri.parse(AppUrl.worldStatesApi));
+    if (response.statusCode == 200) {
+      var data = jsonDecode(response.body.toString());
+      return WorldStatesModel.fromJson(data);
+    } else {
+      throw Exception('Error');
+    }
+  }
+}
